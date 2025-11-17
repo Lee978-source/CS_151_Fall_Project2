c201fb2071c10522068c18fbf1832ab83235a3db
Ethan Le <enathanle@gmail.com>
2025-11-17 00:54:27 -0800

    Created new Main Menu scene for when the player successfully logs into an account or created a new account.

--- gamestate/GameManager.java
+++ gamestate/GameManager.java
@@ -19,6 +19,7 @@ import javafx.stage.Stage;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.Scene;
 
@@ -29,6 +30,7 @@ public class GameManager extends Application {
 	private Scene introScreen; 
 	private Scene loginScreen; 
 	private Scene createAccScreen; 
+	private Scene mainScreen; 
 	
 	// Global Instance UI components to be used in "Log In" and "Create Account" screens (need separate UI components for each one because a node can have ONLY one parent): 
 	private Label loginMessageLabel = new Label(""); // Shows success/failure message ("Log In" screen). 
@@ -39,8 +41,12 @@ public class GameManager extends Application {
 	private TextField createPasswordField = new TextField(); // Create a text field to enter password ("Create Account" screen).
 	
 	// Global Accounts List and Path (to .txt file) to hold all usernames and passwords:
-	private List<String> allUsers = new ArrayList<>(); // Holds ALL players' username and password (in each index). 
+	private List<String> allUsers; // Holds ALL players' username and password (in each index). 
 	private Path userAccountsPath = Paths.get("game_state/user_accounts.txt"); // Get path to the location of the user_accounts.txt file (does NOT know details of the file yet). 
+	
+	// Global Instance Variables to track current player:
+	private String username; 
+	private String password; 
 	
     @Override 
     public void start(Stage primaryStage) { 
@@ -54,10 +60,8 @@ public class GameManager extends Application {
         	this.introScreen = getIntroScreen(); // Create Scene object for this "Intro" screen to be displayed. 
         	this.loginScreen = getLoginScreen(); // Create Scene object for this "Login" screen to be displayed. 
         	this.createAccScreen = getCreateAccScreen(); // Create Scene object for this "Create Account" screen to be displayed. 
+        	this.mainScreen = getMainScreen(); // Create Scene object for this "Main Menu" screen to be displayed. 
         	
-        	//VBox layout = new VBox(10, loginButton, createButton);  // Format layout for "Intro" screen to choose between login or create account buttons. 
-        	//layout.setAlignment(Pos.CENTER); // Align the UI components of the "Intro" screen to the center. 
-        	//Scene scene = new Scene(this.introScreen); // Create Scene object for this "Intro" screen to be displayed. 
         	this.primaryStage.setTitle("Awesome Game Paradise!"); // Title of the Stage (on "Intro" page).
         	this.primaryStage.setScene(this.introScreen); // Set this "Intro" screen as the displayed scene. 
         	this.primaryStage.show(); // Raise the stage curtains! 
@@ -67,8 +71,6 @@ public class GameManager extends Application {
     	{
     		System.out.println("One or more of the data files does not exist or is not readable.");
     	}
-    	
-    	
     }
     
     /** Create and get the "Intro" Screen: **/
@@ -115,13 +117,16 @@ public class GameManager extends Application {
 				
 				for (String user : this.allUsers)
 				{
-					if (user.equals(username + "\t" + password))
+					if (user.equals(username + "\t" + password)) // If the player successfully logs in, set the scene to the "Main Menu" scene. 
 					{
-						System.out.println("Welcome, " + username); // Premature test line to ensure login works. 
+						this.username = username; // Assign the entered username as the current username being used in the program. 
+						this.password = password; // Assign the entered password as the current password being used in the program. 
+						this.primaryStage.setScene(this.mainScreen); 
+						this.primaryStage.setTitle("Welcome, " + username + "! What game would you like to play today?"); // Change the primaryStage title to welcome the user. 
 					}
 				}
 				
-				System.out.println("Failure!"); // For debugging. 
+				//System.out.println("Failure!"); // For debugging. 
 			}
 			catch (IOException e1)
 			{
@@ -165,6 +170,13 @@ public class GameManager extends Application {
 						this.allUsers, // Retrieve the List containing the different "lines" for each player's username and their respective password. 
 						StandardOpenOption.CREATE, // Create the new user_accounts.txt file if it does not exist already. 
 						StandardOpenOption.TRUNCATE_EXISTING); // Otherwise, overwrite the existing user_accounts.txt file every time a new account is created. 
+				
+				// Once the account has been validated and thus created, 
+				this.username = username; // Assign the entered username as the current username being used in the program. 
+				this.password = password; // Assign the entered password as the current password being used in the program. 
+				this.primaryStage.setScene(this.mainScreen); // After validating creation of new account, open the "Main Menu" screen. 
+				this.primaryStage.setTitle("Welcome, " + username + "! What game would you like to play today?"); // Change the primaryStage title to welcome the user. 
+				
 			} catch (IOException e1) {
 				System.out.println("Error reading/writing file: " + e1.getMessage()); // Print message if we get an Input/Output exception with reading/writing the .txt file. 
 			} 
@@ -181,6 +193,35 @@ public class GameManager extends Application {
 		Scene createAccScene = new Scene(createAccLayout, 600, 600); // Create Scene object for "Create Account" screen to be displayed. 
     	
     	return createAccScene; // Return the createAccScene to be assigned to this.loginScene Global Scene. 
+    }
+    
+    /** Create and get the "Main Menu" Screen: **/
+    public Scene getMainScreen()
+    {
+    	// Set up UI components upon loading "Main Menu" screen (the side providing the top 5 high scores):
+    	Label highScoresBJ = new Label("Top 5 high scores for BlackJack:"); 
+    	Label highScoresSnake = new Label("Top 5 high scores for Snake Game:"); 
+    	
+    	// Set up UI components upon loading "Main Menu" screen (the side providing game options):
+    	Button launchBlackJack = new Button("Play BlackJack"); // Button to launch the BlackJack game. 
+    	Button launchSnake = new Button("Play Snake Game"); // Button to launch the Snake game. 
+    	Button empty1 = new Button("New Game TBA"); // Empty button for future game. 
+    	Button empty2 = new Button("New Game TBA"); // Empty button for future game. 
+    	
+    	// Layout for the side providing the top 5 high scores:
+    	VBox scoresLayout = new VBox(10, highScoresBJ, highScoresSnake); // Put components into a Vertical Box. 
+    	scoresLayout.setAlignment(Pos.CENTER); // Align the UI components to the center. 
+    	
+    	// Layout for the side providing game options: 
+    	VBox gamesButtonsLayout = new VBox(10, launchBlackJack, launchSnake, empty1, empty2); // Put components into a Vertical Box. 
+    	gamesButtonsLayout.setAlignment(Pos.CENTER); // Align UI components to the center. 
+    	
+    	// Overall screen layout to hold the scoresLayout VBox and the gamesButtonsLayout VBox: 
+    	HBox mainLayout = new HBox(100, scoresLayout, gamesButtonsLayout); // Put components into a Horizontal Box. 
+    	mainLayout.setAlignment(Pos.CENTER);
+    	Scene mainScreen = new Scene(mainLayout, 600, 600); // Create Scene object for this "Main Menu" screen to be displayed. 
+    	
+    	return mainScreen; // Return the mainScreen to be assigned to this.mainScreen Global Scene. 
     }
    
     /** Main method to launch the JavaFX program: **/ 
